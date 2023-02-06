af3f0b906838cda68aa4dc47544b2acc74e0cfd1
Юрий <fedejkin@gmail.com>
2023-02-06 16:08:48 +0300

    Commit-hw2.0-6

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,13 @@ public class Main {
       flog = flog + 4;
       System.out.println("flog = " + flog);
 
-    }
+      var weight1 = 78.2;
+      System.out.println("weight1 = " + weight1);
+      var weight2 = 82.7;
+      System.out.println("weight2 = " + weight2);
+      var weight3 = weight1 + weight2;
+      System.out.println("weight3 = " + weight3);
+      var weight4 = weight2 % weight1;
+      System.out.println("weight4 = " + weight4);
+   }
 }
