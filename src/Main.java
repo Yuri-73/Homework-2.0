2ec1dd371f0701c378e50933e7015d868f5fdbca
Юрий <fedejkin@gmail.com>
2023-02-06 15:23:48 +0300

    Commit-hw2.0-1

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
       System.out.println("dog = " + dog);
       System.out.println("cat = " + cat);
       System.out.println("paper = " + paper);
+      dog = dog + 4;
+      cat = cat + 4;
+      paper = paper + 4;
+      System.out.println("dog = " + dog);
+      System.out.println("cat = " + cat);
+      System.out.println("paper = " + paper);
     }
 }
